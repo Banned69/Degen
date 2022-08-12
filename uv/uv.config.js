3e3fd5deb39976b6b912adfcb741d046d7c3a26c
Degenerate <82691315+Degen-dev@users.noreply.github.com>
2022-08-12 02:23:53 +0000

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://bare.xmr.bar/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
